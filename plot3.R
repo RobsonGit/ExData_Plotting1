397324d1a552b9e803231f7f344d6b63eac2967d
Robson <ro_box@interia.pl>
2014-08-09 23:23:00 +0200

    New files

--- plot3.R
+++ plot3.R
@@ -13,6 +13,6 @@ plot(data$datetime, data$Sub_metering_1, xlab=NA, ylab="Energy sub metering", ty
 lines(data$datetime, data$Sub_metering_2, type="l", col="red")
 lines(data$datetime, data$Sub_metering_3, type="l", col="blue")
 legend("topright", c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), col=c("black","red","blue"), lty=1)
-dev.copy(png, file="plot2.png")
+dev.copy(png, file="plot3.png")
 dev.off()
 dev.off()
